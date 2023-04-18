0a52f2cdf3d4a1177a713fbac7443bd2eb2ea309
SpectralPixel <encrypteddvjjrxv@gmail.com>
2023-04-19 00:11:27 +0200

    it didn't commit???

--- Assets/Scripts/SnakeManager.cs
+++ Assets/Scripts/SnakeManager.cs
@@ -1,54 +1,62 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SnakeManager : MonoBehaviour
 {
     public static SnakeManager Instance;
 
-    [SerializeField] private int _playerCount;
     public static int SnakeCount;
-    [SerializeField] private uint _defaultSnakeLength;
     public static uint DefaultSnakeLength;
-    [SerializeField] private float _defaultMoveSpeed;
     public static float MoveSpeed;
-    [SerializeField] uint _pointsToGrow;
     public static uint PointsToGrow;
+    public static float MoveCooldown;
+    public static float PickupRadius;
+    public static LayerMask CollectibleLayerMask;
+    public static int PlayerLayer;
+    public static Sprite[] PowerupSprites;
+    public static Sprite[] WeakPowerupSprites;
+
+    [SerializeField] private int _playerCount;
+    [SerializeField] private uint _defaultSnakeLength;
+    [SerializeField] private float _defaultMoveSpeed;
+    [SerializeField] uint _pointsToGrow;
+    [SerializeField] private float _moveCooldown;
+    [SerializeField] private float _defaultPickupRadius;
+    [SerializeField] private LayerMask _collectibleLayerMask;
+    [SerializeField] private int _playerLayer;
+    [SerializeField] private Sprite[] _powerupSprites;
+    [SerializeField] private Sprite[] _weakPowerupSprites;
 
     [SerializeField] private GameObject _snakePrefab;
 
     private void Awake()
     {
-        Instance = this;
-        GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
-    }
+        SnakeCount = _playerCount;
+        DefaultSnakeLength = _defaultSnakeLength;
+        PointsToGrow = _pointsToGrow;
+        PickupRadius = _defaultPickupRadius;
 
-    private void OnDestroy()
-    {
-        GameManager.OnGameStateChanged -= GameManagerOnGameStateChanged;
-    }
+        MoveSpeed = _defaultMoveSpeed;
+        MoveCooldown = _moveCooldown;
 
-    private void GameManagerOnGameStateChanged(GameState state)
-    {
-        Debug.Log("State chenge detected by SnakeManager");
+        CollectibleLayerMask = _collectibleLayerMask;
+        PlayerLayer = _playerLayer;
+
+        PowerupSprites = _powerupSprites;
+        WeakPowerupSprites = _weakPowerupSprites;
     }
 
     private void Start()
     {
-        SnakeCount = _playerCount;
-        DefaultSnakeLength = _defaultSnakeLength;
-        MoveSpeed = _defaultMoveSpeed;
-        PointsToGrow = _pointsToGrow;
-
         for (int i = 0; i < SnakeCount; i++)
         {
             GameObject snake = _snakePrefab;
 
+            Snake snakeScript = snake.GetComponent<Snake>();
             SnakeMovement snakeMovement = snake.GetComponent<SnakeMovement>();
-            snakeMovement.PlayerID = i;
-            snakeMovement.PlayerCount = _playerCount;
+            SnakeHand snakeHand = snake.GetComponent<SnakeHand>();
+
+            snakeScript.PlayerID = i;
+            snakeScript.PlayerCount = _playerCount;
 
             Camera camera = snake.GetComponentInChildren<Camera>();
             Vector3 snakePosition = Vector3.zero;
@@ -56,8 +64,18 @@ public class SnakeManager : MonoBehaviour
             {
                 case 0:
                     snakePosition = new Vector3(0f, 1f, 0f);
-                    snakeMovement.InitialVelocityOverride = new Vector2(0f, 1f);
-                    snakeMovement._keybinds = new KeyCode[] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Q, KeyCode.E, KeyCode.E };
+                    snakeMovement.SpawnVelocity = new Vector2(0f, 1f);
+                    snakeMovement._moveKeys = new KeyCode[]
+                    {
+                        KeyCode.W,
+                        KeyCode.A,
+                        KeyCode.S,
+                        KeyCode.D
+                    };
+
+                    snakeHand.MainPowerupKey = KeyCode.E;
+                    snakeHand.MainPowerupAlt = KeyCode.E;
+                    snakeHand.WeakPowerupKey = KeyCode.Q;
 
                     if (SnakeCount == 1) camera.rect = new Rect(0f, 0f, 1f, 1f);
                     if (SnakeCount == 2) camera.rect = new Rect(0f, 0f, 0.4975f, 1f);
@@ -66,8 +84,18 @@ public class SnakeManager : MonoBehaviour
 
                 case 1:
                     snakePosition = new Vector3(1f, 0f, 0f);
-                    snakeMovement.InitialVelocityOverride = new Vector2(1f, 0f);
-                    snakeMovement._keybinds = new KeyCode[] { KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.RightArrow, KeyCode.RightControl, KeyCode.RightShift, KeyCode.RightShift };
+                    snakeMovement.SpawnVelocity = new Vector2(1f, 0f);
+                    snakeMovement._moveKeys = new KeyCode[]
+                    {
+                        KeyCode.UpArrow,
+                        KeyCode.LeftArrow,
+                        KeyCode.DownArrow,
+                        KeyCode.RightArrow
+                    };
+
+                    snakeHand.MainPowerupKey = KeyCode.RightControl;
+                    snakeHand.MainPowerupAlt = KeyCode.RightAlt;
+                    snakeHand.WeakPowerupKey = KeyCode.RightShift;
 
                     if (SnakeCount == 2) camera.rect = new Rect(0.5025f, 0f, 0.4975f, 1f);
                     if (SnakeCount > 2) camera.rect = new Rect(0.5025f, 0f, 0.4975f, 0.495f);
@@ -75,16 +103,36 @@ public class SnakeManager : MonoBehaviour
 
                 case 2:
                     snakePosition = new Vector3(0f, -1f, 0f);
-                    snakeMovement.InitialVelocityOverride = new Vector2(0f, -1f);
-                    snakeMovement._keybinds = new KeyCode[] { KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.U, KeyCode.O, KeyCode.O };
+                    snakeMovement.SpawnVelocity = new Vector2(0f, -1f);
+                    snakeMovement._moveKeys = new KeyCode[]
+                    {
+                        KeyCode.I,
+                        KeyCode.J,
+                        KeyCode.K,
+                        KeyCode.L
+                    };
+
+                    snakeHand.MainPowerupKey = KeyCode.O;
+                    snakeHand.MainPowerupAlt = KeyCode.O;
+                    snakeHand.WeakPowerupKey = KeyCode.U;
 
                     camera.rect = new Rect(0f, 0.505f, 0.498f, 0.495f);
                     break;
 
                 case 3:
                     snakePosition = new Vector3(-1f, 0f, 0f);
-                    snakeMovement.InitialVelocityOverride = new Vector2(-1f, 0f);
-                    snakeMovement._keybinds = new KeyCode[] { KeyCode.T, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.R, KeyCode.Y, KeyCode.Z };
+                    snakeMovement.SpawnVelocity = new Vector2(-1f, 0f);
+                    snakeMovement._moveKeys = new KeyCode[]
+                    {
+                        KeyCode.T,
+                        KeyCode.F,
+                        KeyCode.G,
+                        KeyCode.H
+                    };
+
+                    snakeHand.MainPowerupKey = KeyCode.Y;
+                    snakeHand.MainPowerupAlt = KeyCode.Z;
+                    snakeHand.WeakPowerupKey = KeyCode.R;
 
                     camera.rect = new Rect(0.5025f, 0.505f, 0.4975f, 0.495f);
                     break;
